4a8c5159d2efeacbd89999f29a78cc404e7efbe1
Fayne <FaynePerera@users.noreply.github.com>
2023-08-14 09:12:15 -0600

    Delete hooks:useCheckSession.js

--- hooks:useCheckSession.js
+++
@@ -1,8 +0,0 @@
-import { useSession } from 'next-auth/react';
-
-export default function useCheckSession() {
-  const { status } = useSession();
-  const isLoggedIn = status === 'authenticated';
-
-  return isLoggedIn;
-}
